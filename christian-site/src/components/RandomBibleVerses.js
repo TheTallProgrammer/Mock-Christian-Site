e0ba6a85b7f22f8d49ab6530925ba76f88f6f451
Logan Falkenberg <llf46@txstate.edu>
2024-12-22 21:49:39 -0600

    added button to fetch random verses from endpoint

---
+++ christian-site/src/components/RandomBibleVerses.js
@@ -0,0 +1,40 @@
+import react, {useState, useEffect} from 'react'
+
+const RandomBibleVerses = () => {
+    
+    const [verse, setVerse] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const fetchVerse = async () => {
+        setLoading(true);
+        const res = await fetch('https://labs.bible.org/api/?passage=random&type=json');
+        const data = await res.json();
+        setVerse(data[0]);
+        setLoading(false);
+    }
+
+    // on initial mount call function to fetch the verse
+    useEffect( () => {
+        fetchVerse();
+    }, []);
+
+    return ( 
+        <div className="verses-container">
+             {loading ? (
+                <p>Loading...</p>
+             ) : (
+                verse && (
+                    <p>
+                        <strong>
+                            {verse.bookname} {verse.chapter}:{verse.verse}
+                        </strong>{' '}
+                        {verse.text}
+                    </p>
+                )
+             )}
+             <button onClick={fetchVerse}>New Verse</button>
+        </div>
+     );
+}
+ 
+export default RandomBibleVerses;
