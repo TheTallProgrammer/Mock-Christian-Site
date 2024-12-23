f57264f8761aba670b55ad01a1a5e56fe8a0e59e
Logan Falkenberg <llf46@txstate.edu>
2024-12-22 22:13:49 -0600

    made some changes

--- christian-site/src/App.js
+++ christian-site/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
+import crossesImage from './assets/crosses.png';
 import RandomBibleVerses from './components/RandomBibleVerses';
 
 function App() {
   return (
     <div className="App">
-      <div className='background-wrapper'>
-        <video src={'https://res.cloudinary.com/dfbqfdbxw/video/upload/v1734923172/4933875-uhd_4096_2160_30fps_qquhnf.mp4'} autoPlay loop muted />
+      <div className="background-wrapper">
+        <video
+          src={
+            'https://res.cloudinary.com/dfbqfdbxw/video/upload/v1734923172/4933875-uhd_4096_2160_30fps_qquhnf.mp4'
+          }
+          autoPlay
+          loop
+          muted
+        />
       </div>
-      <RandomBibleVerses/>
+      <RandomBibleVerses />
+      <div className='image-container'>
+        <img src={crossesImage} alt="Crosses" />
+      </div>
     </div>
   );
 }
