cc0b6f6aceb7b2c3cc5b89cef94eef12ef102fc9
Logan Falkenberg <llf46@txstate.edu>
2024-12-22 21:47:49 -0600

    Have video playing in background now

--- christian-site/src/App.js
+++ christian-site/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='background-wrapper'>
+        <video src={'https://res.cloudinary.com/dfbqfdbxw/video/upload/v1734923172/4933875-uhd_4096_2160_30fps_qquhnf.mp4'} autoPlay loop muted />
+      </div>
       <p>Mock Christian Site</p>
     </div>
   );
